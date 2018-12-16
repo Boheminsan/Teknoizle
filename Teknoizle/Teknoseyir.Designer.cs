bc15600276456360b4c67100f70fdcb45d0e61c2
Batuhan Aktaş <aktas.batu@hotmail.com>
2018-12-17 02:05:11 +0300

    Proje dosyası ekle.

---
+++ Teknoizle/Teknoseyir.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Teknoizle
+{
+    partial class Teknoseyir
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 80);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(129, 41);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "Modal Atla";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 195);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(439, 355);
+            this.listBox1.TabIndex = 2;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(178, 80);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(129, 41);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Aşağı Kaydır";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(330, 80);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(129, 41);
+            this.metroButton3.TabIndex = 4;
+            this.metroButton3.Text = "ListBox\'a Ekle";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(23, 136);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(129, 41);
+            this.metroButton4.TabIndex = 5;
+            this.metroButton4.Text = "Daha Fazla Göster";
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Location = new System.Drawing.Point(178, 136);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(129, 41);
+            this.metroButton5.TabIndex = 6;
+            this.metroButton5.Text = "Dosyaya Kaydet";
+            this.metroButton5.UseSelectable = true;
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(404, 162);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(55, 15);
+            this.metroCheckBox1.TabIndex = 7;
+            this.metroCheckBox1.Text = "Kapalı";
+            this.metroCheckBox1.UseSelectable = true;
+            this.metroCheckBox1.CheckedChanged += new System.EventHandler(this.metroCheckBox1_CheckedChanged);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(330, 137);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(125, 19);
+            this.metroLabel1.TabIndex = 8;
+            this.metroLabel1.Text = "Otomatik kaydırma:";
+            // 
+            // Teknoseyir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 570);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroCheckBox1);
+            this.Controls.Add(this.metroButton5);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Name = "Teknoseyir";
+            this.Text = "TeknoSeyir";
+            this.Load += new System.EventHandler(this.Teknoseyir_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
+
---
+++ Teknoizle/Teknoseyir.cs
@@ -0,0 +1,156 @@
+﻿using MetroFramework.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Teknoizle
+{
+    public partial class Teknoseyir : MetroForm
+    {
+        public Teknoseyir()
+        {
+            InitializeComponent();
+        }
+
+        string url;
+        IWebDriver driverts = new EdgeDriver();
+
+        private void Teknoseyir_Load(object sender, EventArgs e)
+        {
+            url = "https://www.teknoseyir.com/videolar/";
+            driverts.Navigate().GoToUrl(url);
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                driverts.FindElement(By.CssSelector("#onesignal-popover-cancel-button")).Click();
+                //IWebElement garba =driver.FindElement(By.Id("onesignal-popover-cancel-button"));
+                driverts.FindElement(By.CssSelector("#genelModal")).Click();
+                //var abc=driver.FindElement(By.Id("onesignal-popover-container"));
+                //MessageBox.Show(abc.ToString());
+            }
+            catch (NoSuchElementException)
+            {
+                Thread.Sleep(500);
+            }
+            catch (ElementNotInteractableException)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            if (metroCheckBox1.Checked == true)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    kaydir();
+                }
+            }
+            else
+            {
+                kaydir();
+            }
+        }
+
+        private void kaydir()
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driverts;
+            js.ExecuteScript("window.scrollBy(0,1200)", "");
+            try
+            {
+                var abc = driverts.FindElement(By.CssSelector("body > main > btn"));
+                //MessageBox.Show(abc.Text);
+                //if (abc.Text != "")
+                //{
+                //    MessageBox.Show("Test");
+                //}
+            }
+            catch (NoSuchElementException)
+            {
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            ReadOnlyCollection<IWebElement> lists1 = driverts.FindElements(By.CssSelector("div.content > h1 > a"));
+            int lcnt = lists1.Count;
+            MessageBox.Show(lcnt.ToString());
+            for (int i = lcnt - 1; i >= 0; i--)
+            {
+                listBox1.Items.Add(lists1[i].GetAttribute("href"));
+                listBox1.Items.Add(lists1[i].Text);
+            }
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                driverts.FindElement(By.CssSelector("body > main > btn")).Click();
+            }
+            catch (NoSuchElementException)
+            {
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driverts;
+                js.ExecuteScript("window.scrollBy(0,600)", "");
+                Thread.Sleep(500);
+            }
+        }
+
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            int sayac = 1;
+            int ksayac = 2;
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.CreatePrompt = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string item = "";
+                StreamWriter Kayit = new StreamWriter(sfd.FileName);
+                string onsatir = "<DT><H3 ADD_DATE=\"1515536908\" LAST_MODIFIED=\"1531485215\">TS</H3>";
+                Kayit.WriteLine(onsatir);
+                string satir1 = "<DL><p>";
+                Kayit.WriteLine(satir1);
+                string satir2 = "<DT><H3 ADD_DATE=\"1531485215\" LAST_MODIFIED=\"1531485215\">TS1</H3>";
+                Kayit.WriteLine(satir2);
+                string satir3 = "</DL><p>";
+                Kayit.WriteLine(satir1);
+                for (int i = 0; i < listBox1.Items.Count; i += 2)
+                {
+                    if (sayac < 10)
+                    {
+                        item = "<DT><A HREF=\"" + listBox1.Items[i] + "\" ADD_DATE=\"1531485215\">" + listBox1.Items[i + 1] + "</A>";
+                        Kayit.WriteLine(item);
+                        sayac++;
+                    }
+                    else
+                    {
+                        Kayit.WriteLine(satir3);
+                        satir2 = "<DT><H3 ADD_DATE=\"1531485215\" LAST_MODIFIED=\"1531485215\">TS" + ksayac + "</H3>";
+                        Kayit.WriteLine(satir2);
+                        Kayit.WriteLine(satir1);
+                        ksayac++;
+                        sayac = 0;
+                    }
+
+                }
+                Kayit.Close();
+            }
+        }
+
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            metroCheckBox1.Text = "Açık";
+        }
+    }
+}
